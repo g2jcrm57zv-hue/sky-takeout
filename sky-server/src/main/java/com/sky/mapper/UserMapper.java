057142729f70bc66141e33edb50cf3267faa941a
cheny_windows <sl20113321@163.com>
2026-04-10 03:51:23 +0800

    完成day11的用户统计功能

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,9 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -25,4 +28,6 @@ public interface UserMapper {
      */
     @Select("select * from user where id=#{userId}")
     User getById(Long userId);
+
+    Integer countByMap(Map map);
 }
